5d0922bc05bb91145168aabde57b73892740a0c9
GSvenny <gabrielswagner7@gmail.com>
2022-09-28 18:53:23 -0500

    basic server setup/keeping basic for now

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 const express = require ('express');
+require('dotenv').config()
 
+// express app
 const app = express();
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+//middleware
+app.use((req, res, next) => {
+console.log(req.path, req.method)
+next()
+})
+
+//routes
+app.get('/', (req,res) => {
+    res.json({mssg: "test"})
+})
+
+
+//PORT 
+app.listen(process.env.PORT, () => {
+    console.log('listening on port 3000', process.env.PORT)
 })
