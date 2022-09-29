4bda6f8f4511bfdbef26bb4b546c4124b89ea073
GSvenny <gabrielswagner7@gmail.com>
2022-09-29 12:12:13 -0500

    reinstalled react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-//components
-import Home from './pages/Home'
-import Nav from './components/Nav'
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Nav />
-      <div className='pages'>
-        <Routes>
-          <Route 
-          path='/'
-          element={<Home />}
-          />
-        </Routes>
-      </div>
-      </BrowserRouter>
-    </div>
+    <div><h1>Group Project 3</h1></div>
   );
 }
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,7 +25,6 @@ const resolvers = {
       
               return trip;
             }
-
     }
   }
 };
