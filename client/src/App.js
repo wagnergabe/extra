d684f1f3e0d76af7021ef4b27bc0c443996ac875
GSvenny <gabrielswagner7@gmail.com>
2022-09-29 00:25:20 -0500

    frontend and backend connected

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //components
 import Home from './pages/Home'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav />
       <div className='pages'>
         <Routes>
           <Route 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+// components
+import TripInfo from '../components/TripInfo'
+
 const Home = () => {
+    const [trips, setTrips] = useState(null)
+
+    useEffect(() => {
+        const fetchTrips = async () => {
+            const response = await fetch('/api/trips')
+            const json = await response.json() 
+
+            if (response.ok) {
+                setTrips(json)
+            }
+        }
+
+        fetchTrips()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Group Project 3</h2>
+            <div className="trips"></div>
+            {trips && trips.map((trip) => (
+                <TripInfo key = {trip._id} trip ={trip} />
+            ))}
         </div>
     )
 }
