bf739ffe01fe543fc5258c2cbcb624bc6bc99adf
Miguel Soriano <originalsorianom@gmail.com>
2022-10-03 02:10:22 -0700

    adding login in mutation inside model/resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -94,7 +94,20 @@ const resolvers = {
       const user = await User.create(args);
       const token = signToken(user);
       return {token, user};
+    },
+    login: async (parent, {email, password}) => {
+      const user = await User.findOne({email});
+      if (!user) {
+        throw new AuthenticationError('incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword (password);
 
+      if (!correctPw) {
+        throw new AuthenticationError('incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return {token, user};
     }
 
   },
