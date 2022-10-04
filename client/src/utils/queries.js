bb1152f06c4b7220807ebeda0ed2d43539393d64
Tricia <tricianghiem@gmail.com>
2022-10-04 18:54:23 -0500

    added comments query for post

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,23 +1,34 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_POSTS = gql `
-    query posts($username: String!) {
-        posts(username: $username) {
-            _id
-            postTitle
-            postText
-            username
+export const QUERY_POSTS = gql`
+  query posts($username: String!) {
+    posts(username: $username) {
+      _id
+      postTitle
+      postText
+      username
     }
-}
+  }
 `;
 
-export const QUERY_POST = gql `
-    query post($id: ID!) {
-        post(_id: $id) {
-            _id
-            postTitle
-            postText
-            username
-        }
+export const QUERY_POST = gql`
+  query post($id: ID!) {
+    post(_id: $id) {
+      _id
+      postTitle
+      postText
+      username
     }
+  }
+`;
+
+export const QUERY_COMMENTS = gql`
+  query comments($post_id: ID!) {
+    comments(filter: ) {
+      _id
+      commentContent
+      commentDate
+      commentUser
+    }
+  }
 `;
