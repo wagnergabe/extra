d482a8158dad84d4a6614e38c9a6d2fa7cad2d95
Miguel Soriano <originalsorianom@gmail.com>
2022-10-05 03:17:19 -0700

    update file

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//components
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+// components
+import TripInfo from "../components/TripInfo";
+import TripForm from "../components/TripForm";
+
+const Home = () => {
+  const [trips, setTrips] = useState(null);
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      const response = await fetch("/api/trips");
+      const json = await response.json();
+
+      if (response.ok) {
+        setTrips(json);
+      }
+    };
+
+    fetchTrips();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="trips">
+        {trips && trips.map((trip) => <TripInfo key={trip._id} trip={trip} />)}
+      </div>
+      <TripForm />
+    </div>
+  );
+};
+
+export default Home;
