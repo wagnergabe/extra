2e4c6b5c2d959f0b7877f79406c3123801114838
Tricia <tricianghiem@gmail.com>
2022-10-05 18:06:30 -0500

    removed login and sign up pages

--- client/src/pages/Login.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useMutation } from "@apollo/client";
-import auth from "../utils/auth";
-import { LOGIN_USER } from "../utils/mutations";
-
-export default function Login() {}
